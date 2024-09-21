3f5cc9bca3503a9613dad6c58c92389bad166ea3
Sam Swain <sam@apparance.uk>
2024-09-21 10:53:42 +0100

    Free RTS camera plugin

---
+++ Plugins/OpenRTSCamera/Source/OpenRTSCamera/Public/RTSCameraBoundsVolume.h
@@ -0,0 +1,15 @@
+// Copyright 2024 Jesus Bracho All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CameraBlockingVolume.h"
+#include "RTSCameraBoundsVolume.generated.h"
+
+UCLASS()
+class OPENRTSCAMERA_API ARTSCameraBoundsVolume : public ACameraBlockingVolume
+{
+	GENERATED_BODY()
+
+	ARTSCameraBoundsVolume();
+};
---
+++ Plugins/OpenRTSCamera/Source/OpenRTSCamera/Public/RTSSelectable.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "RTSSelectable.generated.h"
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class OPENRTSCAMERA_API URTSSelectable : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "RTS Selection")
+	void OnSelected();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "RTS Selection")
+	void OnDeselected();
+};
